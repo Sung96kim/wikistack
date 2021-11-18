ae7a26386a57e91090d68f40094de8a4036bdd8b
Sung <sung96kim@gmail.com>
2021-11-18 16:27:35 -0500

    ADD redirect to homepage

--- api/wiki.js
+++ api/wiki.js
@@ -1,3 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const client = require("../models")
+const { addPage } = require("../views")
+const layout = require('../views/layout')
+
+router.get('/', async (req, res, next) => {
+  try {
+    res.send(layout(''))
+  }catch(err){
+    next(err)
+  }
+})
+
+router.post('/', async(req, res, next) => {
+  try{
+    console.log('POST REQUEST')
+  }catch(err){
+    next(err)
+  }
+})
+
+router.get('/add', async(req, res, next) => {
+  try{
+    res.send(addPage())
+  }catch(err){
+    next(err)
+  }
+})
+
+module.exports = router;
 
--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const morgan = require('morgan')
 const app = express()
 // const views = require('./views')
-const layout = require('./views/layout')
-const {db, Page, User} = require('./models')
+const { db, Page, User } = require('./models')
 
 const PORT = 8080;
 
@@ -11,6 +10,8 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use(morgan('dev'))
+app.use(require('./api'))
+
 app.use(express.static(__dirname + "/public"));
 
 db.authenticate().then(()=>{
@@ -18,7 +19,11 @@ db.authenticate().then(()=>{
 })
 
 app.get('/', (req, res)=>{
-  res.send(layout(''))
+  res.redirect('/wiki')
+})
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>PAGE NOT FOUND</h1>')
 })
 
 const start = async () => {
